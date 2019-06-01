d685fc1469f73949ec76331e78a27d8b767c65b6
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-01 16:19:27 +0300

    Add files via upload

---
+++ Lesson 2/09. Metric Converter .cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _09._Metric_Converter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            string firstMeasure = Console.ReadLine();
+            string secondMeasure = Console.ReadLine();
+            var measure = new Dictionary<string, double>()
+            {
+                { "m", 1},
+                { "mm", 1000},
+                { "cm", 100},
+                { "mi", 0.000621371192},
+                { "in", 39.3700787},
+                { "km", 0.001},
+                { "ft", 3.2808399},
+                { "yd", 1.0936133},
+            };
+            double result = number / measure[firstMeasure] * measure[secondMeasure];
+            Console.WriteLine($"{result:f8}");
+        }
+    }
+}
