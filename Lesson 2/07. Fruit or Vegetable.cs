844e5cba35461f252ce56ebf04c14eebc147c322
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-01 16:18:50 +0300

    Add files via upload

---
+++ Lesson 2/07. Fruit or Vegetable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _07._Fruit_or_Vegetable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] fruits = { "banana", "apple", "kiwi","cherry", "lemon", "grapes"};
+            string[] vegetables = {"tomato", "cucumber", "pepper","carrot"};
+
+            string input = Console.ReadLine();
+
+            if (fruits.Contains(input))
+            {
+                Console.WriteLine("fruit");
+            }
+            else if (vegetables.Contains(input))
+            {
+                Console.WriteLine("vegetable");
+            }
+            else
+            {
+                Console.WriteLine("unknown");
+            }
+        }
+    }
+}
