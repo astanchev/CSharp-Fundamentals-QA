1a3a964455a87329fd401cee1b838129d9f2c955
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-01 16:20:47 +0300

    Add files via upload

---
+++ Lesson 2/12. Time plus 15 Minutes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _12._Time_plus_15_Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            int allMinutes = hours * 60 + minutes + 15;
+
+            int currentHours = allMinutes / 60;
+            int currentMinutes = allMinutes % 60;
+
+            if (currentHours > 23)
+            {
+                currentHours -= 24;
+            }
+
+            Console.WriteLine($"{currentHours}:{currentMinutes:00}");
+        }
+    }
+}
