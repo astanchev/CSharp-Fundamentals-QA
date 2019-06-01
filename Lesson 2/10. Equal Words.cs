19bc3846860ac0d25139953e734a804eef713b07
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-01 16:19:47 +0300

    Add files via upload

---
+++ Lesson 2/10. Equal Words.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _10._Equal_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word1 = Console.ReadLine()?.ToLower();
+            string word2 = Console.ReadLine()?.ToLower();
+
+            if (word1.Equals(word2))
+            {
+                Console.WriteLine("yes");
+            }
+            else
+            {
+                Console.WriteLine("no");
+            }
+        }
+    }
+}
