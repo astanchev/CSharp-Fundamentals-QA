fb4c177e47dde80a47d1e32c292def5e916b087c
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-03 00:30:28 +0300

    Add files via upload

---
+++ Lesson 2/36. Number Table.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _36._Number_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                int br = 0;
+                for (int col = 0; col < n; col++)
+                {
+                    num = row + col + 1;
+                    if (num > n)
+                    {
+                        num = 2 * n - num;
+                    }
+
+                    if (br > 0)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(num);
+                    br++;
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
