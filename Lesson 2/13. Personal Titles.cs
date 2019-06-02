fad088d897a8e4b8783f4532ed3863a867379257
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-03 00:22:21 +0300

    Add files via upload

---
+++ Lesson 2/13. Personal Titles.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _13._Personal_Titles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double age = double.Parse(Console.ReadLine());
+            string gender = Console.ReadLine();
+
+            if (age < 16)
+            {
+                if (gender=="m")
+                {
+                    Console.WriteLine("Master");
+                }
+                else
+                {
+                    Console.WriteLine("Miss");
+                }
+            }
+            else
+            {
+                if (gender == "m")
+                {
+                    Console.WriteLine("Mr.");
+                }
+                else
+                {
+                    Console.WriteLine("Ms.");
+                }
+            }
+        }
+    }
+}
