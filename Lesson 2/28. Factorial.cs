13de587588658bc2f7a17d4c995a6615d0da9a1c
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-03 00:27:46 +0300

    Add files via upload

---
+++ Lesson 2/28. Factorial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _28._Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int i = 2;
+            decimal sum = 1;
+
+            while (i <= n)
+            {
+                sum = sum * i;
+                i++;
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
