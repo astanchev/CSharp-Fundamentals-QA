7e0a82fa1e9ead9194334aa6b9da3d11d77c044e
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-03 00:24:50 +0300

    Add files via upload

---
+++ Lesson 2/20. Numbers to 1000 Ending 7.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _20._Numbers_to_1000_Ending_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int br = 0;
+            for (int i = 1; i <= 1000; i++)
+            {
+                if (i % 10 == 7)
+                {
+                    if (br > 0)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(i);
+                    br++;
+                }
+            }
+        }
+    }
+}
