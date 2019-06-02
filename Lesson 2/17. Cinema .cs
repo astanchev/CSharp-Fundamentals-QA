bac6a98c2767e280a71d968b4a874576c832054c
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-03 00:23:44 +0300

    Add files via upload

---
+++ Lesson 2/17. Cinema .cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _17._Cinema
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+            double price = 0.0;
+
+            int seats = rows * cols;
+
+            switch (type)
+            {
+                case "Premiere": price = 12.0; break;
+                case "Normal": price = 7.5; break;
+                case "Discount": price = 5.0; break;
+            }
+
+            Console.WriteLine($"{price*seats:f2}");
+        }
+    }
+}
