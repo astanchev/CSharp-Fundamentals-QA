b80fff71ab72b2360d1358f91c661e6dad64bfe5
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-03 00:27:22 +0300

    Add files via upload

---
+++ Lesson 2/27. Sequence 2k plus 1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _27._Sequence_2k_plus_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int num = 1;
+
+            while (true)
+            {
+                if (num > n)
+                {
+                    break;
+                }
+
+                Console.WriteLine(num);
+                num = 2 * num + 1;
+            }
+        }
+    }
+}
