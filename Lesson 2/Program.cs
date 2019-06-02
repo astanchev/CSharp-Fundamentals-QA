a54fe8f12492f33842ddc10bf3fd809fba2561df
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-03 00:29:24 +0300

    Add files via upload

---
+++ Lesson 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _33._Equal_Pairs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int sum = num1 + num2;
+            int maxPairDiff = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                int oldDiff = sum;
+
+                num1 = int.Parse(Console.ReadLine());
+                num2 = int.Parse(Console.ReadLine());
+                sum = num1 + num2;
+                if (sum != oldDiff)
+                {
+                    int currentPairDiff = Math.Abs(sum - oldDiff);
+                    if (currentPairDiff > maxPairDiff)
+                    {
+                        maxPairDiff = currentPairDiff;
+                    }
+                }
+            }
+
+            if (maxPairDiff == 0)
+            {
+                Console.WriteLine($"Yes, value={sum}");
+            }
+            else
+            {
+                Console.WriteLine($"No, maxdiff={maxPairDiff}");
+            }
+        }
+    }
+}
