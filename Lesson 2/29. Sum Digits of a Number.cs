fa6a5300b80710689cb2126cc7847939dcbe7c2d
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-03 00:28:09 +0300

    Add files via upload

---
+++ Lesson 2/29. Sum Digits of a Number.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _29._Sum_Digits_of_a_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while (num != 0)
+            {
+                sum += num % 10;
+                num /= 10;
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
