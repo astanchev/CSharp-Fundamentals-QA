3a75e88f302b0c0c0a6e13b0df9afd224614f30f
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-03 00:29:44 +0300

    Add files via upload

---
+++ Lesson 2/34. Check Prime Number.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _34._Check_Prime_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            bool isPrime = true;
+            if (number > 2)
+            {
+                for (int i = 2; i <= Math.Ceiling(Math.Sqrt(number)); ++i)
+                {
+                    if (number % i == 0) isPrime = false;
+                }
+            }
+
+            if (isPrime && number >= 2)
+            {
+                Console.WriteLine("Prime");
+            }
+            else
+            {
+                Console.WriteLine("Not prime");
+            }
+        }
+    }
+}
