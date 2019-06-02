f2c5cad2ea728b2091b4d9ff444f6e7f081e0fe3
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-03 00:30:09 +0300

    Add files via upload

---
+++ Lesson 2/35. Number Pyramid.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _35._Number_Pyramid
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int counter = 1;
+
+            for (int i = 1; counter <= n; i++)
+            {
+                int br = 0;
+                for (int j = 1; j <= i; j++)
+                {
+                    if (br>0)
+                    {
+                        Console.Write(" ");
+                    }
+                    Console.Write(counter++);
+                    br++;
+                    if (counter > n)
+                    {
+                        return; ;
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
