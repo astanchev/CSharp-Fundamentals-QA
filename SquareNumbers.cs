2bb79a42a9e149ebd2bec5234be0d342e6477aa8
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-05 15:29:35 +0300

    Add files via upload

---
+++ SquareNumbers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _23._Square_Numbers
+{
+    class SquareNumbers
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (Math.Sqrt(input[i]) == (int)Math.Sqrt(input[i]))
+                {
+                    result.Add(input[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result
+                                                        .OrderByDescending(x => x)));
+        }
+    }
+}
