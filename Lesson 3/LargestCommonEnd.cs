b7934230d21b8accb4ddc1576221e55353aa79ac
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-05 15:25:13 +0300

    Add files via upload

---
+++ Lesson 3/LargestCommonEnd.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08._Largest_Common_End
+{
+    class LargestCommonEnd
+    {
+        static void Main(string[] args)
+        {
+            string[] arr1 = Console.ReadLine().Split();
+            string[] arr2 = Console.ReadLine().Split();
+
+            int maxLength = Math.Max(arr1.Length, arr2.Length);
+            int minLength = Math.Min(arr1.Length, arr2.Length);
+
+            int difference = maxLength - minLength; 
+            int startCount = 0;
+            int endCount = 0;
+
+            for (int i = 0; i < minLength; i++)
+            {
+                if (arr1[i] == arr2[i])
+                {
+                    startCount++;
+                }
+                else 
+                {
+                    break;
+                }
+            }
+
+            Array.Reverse(arr1);
+            Array.Reverse(arr2);
+            
+            for (int i = 0; i < minLength; i++)
+            {
+                if (arr1[i] == arr2[i])
+                {
+                    endCount++;
+                }
+                else 
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine(Math.Max(startCount, endCount));
+        }
+    }
+}
---
+++ Lesson 3/RotateandSum.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _09._Rotate_and_Sum
+{
+    class RotateandSum
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int numberRotations = int.Parse(Console.ReadLine());
+            int[] result = new int[array.Length];
+            
+            for (int i = 0; i < numberRotations; i++)
+            {
+                int tempLast = array[array.Length - 1];
+                for (int j = array.Length - 1; j >= 1; j--)
+                {
+                    array[j] = array[j - 1];
+                    result[j] += array[j];
+                }
+                array[0] = tempLast;
+                result[0] += array[0];
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
