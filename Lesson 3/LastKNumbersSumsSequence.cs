d034085c9c632b64dfad6962b47d140deb301efc
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-05 15:24:14 +0300

    Add files via upload

---
+++ Lesson 3/LastKNumbersSumsSequence.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _07._Last_K_Numbers_Sums_Sequence
+{
+    class LastKNumbersSumsSequence
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int k = int.Parse(Console.ReadLine());
+
+            long[] arr = new long[n];
+            arr[0] = 1;
+
+            for (int i = 1; i < n; i++)
+            {
+                long currentSum = 0;
+                int startIndex = -1;
+                if (i - k <= 0)
+                {
+                    startIndex = 0;
+                }
+                else
+                {
+                    startIndex = i - k;
+                }
+
+                for (int j = startIndex; j < i; j++)
+                {
+                    currentSum += arr[j];
+                }
+
+                arr[i] = currentSum;
+            }
+
+            Console.WriteLine(string.Join(" ", arr));
+        }
+    }
+}
---
+++ Lesson 3/SumArrays.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace _04._Sum_Arrays
+{
+    class SumArrays
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] arr2 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int maxLenght = Math.Max(arr1.Length, arr2.Length);
+            int[] result = new int[maxLenght];
+
+            for (int i = 0; i < maxLenght; i++)
+            {
+                if (i > arr1.Length - 1)
+                {
+                    result[i] = arr1[i % arr1.Length] + arr2[i];
+                }
+                else if (i > arr2.Length - 1)
+                {
+                    result[i] = arr2[i % arr2.Length] + arr1[i];
+                }
+                else
+                {
+                    result[i] = arr2[i] + arr1[i];
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
