27452b131c6bcd90c215fc36c1f18397f23e08b6
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-05 15:28:02 +0300

    Add files via upload

---
+++ Lesson 3/EqualSums.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _17._Equal_Sums
+{
+    class EqualSums
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                int leftSum = 0;
+                int rightSum = 0;
+                for (int leftIndex = 0; leftIndex < i; leftIndex++)
+                {
+                    leftSum += array[leftIndex];
+                }
+                for (int rightIndex = i + 1; rightIndex < array.Length; rightIndex++)
+                {
+                    rightSum += array[rightIndex];
+                }
+                if (leftSum == rightSum)
+                {
+                    Console.WriteLine(i);
+                    return;                        
+                }
+            }
+            Console.WriteLine("no");
+        }
+    }
+}
---
+++ Lesson 3/PairsbyDifference.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _16._Pairs_by_Difference
+{
+    class PairsbyDifference
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int difference = int.Parse(Console.ReadLine());
+
+            int counter = 0;
+
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] - array[j] == difference || array[j] - array[i] == difference)
+                    {
+                        counter++;
+                    }
+                }
+            }
+
+            Console.WriteLine($"{counter}");
+        }
+    }
+}
