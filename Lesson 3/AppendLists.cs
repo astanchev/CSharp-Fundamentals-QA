ec81cfeca26a9a8867da83dae562158c12102c6b
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-05 15:28:47 +0300

    Add files via upload

---
+++ Lesson 3/AppendLists.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _19._Append_Lists
+{
+    class AppendLists
+    {
+        static void Main(string[] args)
+        {
+            List<string> arrays = Console.ReadLine()
+                .Split('|')
+                .ToList();
+
+            List<int> result = new List<int>();
+
+            for (int i = arrays.Count - 1; i >= 0 ; i--)
+            {
+                string arrayString = arrays[i];
+                List<int> temp = arrayString
+                    .Split(new char[] {' '} , StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToList();
+                result.AddRange(temp);
+            }
+
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
---
+++ Lesson 3/SplitbyWordCasing.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _21._Split_by_Word_Casing
+{
+    class SplitbyWordCasing
+    {
+        static void Main(string[] args)
+        {
+            List<string> lower = new List<string>();
+            List<string> upper = new List<string>();
+            List<string> mixed = new List<string>();
+
+            char[] separators = { ',', ';', ':', '.',
+                                '!', '(', ')', '\"', '\'',
+                                '\\', '/', '[', ']', ' '};
+
+            string[] input = Console.ReadLine()
+                .Split(separators, StringSplitOptions.RemoveEmptyEntries);
+            
+            foreach (var word in input)
+            {
+                int lowerCases = 0;
+                int upperCases = 0;
+                
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (char.IsUpper(word[i]))
+                    {
+                        upperCases++;
+                    }
+                    if (char.IsLower(word[i]))
+                    {
+                        lowerCases++;
+                    }
+                }
+
+                if (lowerCases == word.Length)
+                {
+                    lower.Add(word);
+                }
+                else if (upperCases == word.Length)
+                {
+                    upper.Add(word);
+                }
+                else
+                {
+                    mixed.Add(word);
+                }
+            }
+
+            Console.WriteLine("Lower-case: " + string.Join(", ", lower));
+            Console.WriteLine("Mixed-case: " + string.Join(", ", mixed));
+            Console.WriteLine("Upper-case: " + string.Join(", ", upper));
+        }
+    }
+}
