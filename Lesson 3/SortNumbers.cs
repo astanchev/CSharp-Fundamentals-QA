2f6897a7e3d2779748d62aeb50e4d2d258b7815e
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-05 15:29:08 +0300

    Add files via upload

---
+++ Lesson 3/SortNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _22._Sort_Numbers
+{
+    class SortNumbers
+    {
+        static void Main(string[] args)
+        {
+            List<double> input = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToList();
+
+            input.Sort();
+            Console.WriteLine(string.Join(" <= ", input));
+        }
+    }
+}
