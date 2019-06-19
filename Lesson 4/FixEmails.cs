57491f08c4de34b2e3afdf9748525b5224227089
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-19 08:40:08 +0300

    Add files via upload

---
+++ Lesson 4/FixEmails.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Fix_Emails
+{
+    class FixEmails
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> nameEmail = new Dictionary<string, string>();
+
+            while (true)
+            {
+                string name = Console.ReadLine();
+                if (name == "stop")
+                {
+                    break;
+                }
+                string email = Console.ReadLine();
+                if (email == "stop")
+                {
+                    break;
+                }
+
+                if (email.EndsWith(".us") || email.EndsWith(".uk"))
+                {
+                    continue;
+                }
+
+                if (!nameEmail.ContainsKey(name))
+                {
+                    nameEmail[name] = email;
+                }
+            }
+
+            if (nameEmail.Count > 0)
+            {
+                foreach (var name in nameEmail)
+                {
+                    Console.WriteLine($"{name.Key} -> {name.Value}");
+                }
+            }
+        }
+    }
+}
