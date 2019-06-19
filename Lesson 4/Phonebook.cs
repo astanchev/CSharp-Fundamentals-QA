827783eb756d6c474717ed1c50a247c2aa6b51dd
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-19 08:39:01 +0300

    Add files via upload

---
+++ Lesson 4/Phonebook.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _03._Phonebook
+{
+    class Phonebook
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> namePhone = new Dictionary<string, string>();
+
+            while (true)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                if (input[0] == "END")
+                {
+                    return;
+                }
+                else if (input[0] == "A")
+                {
+                    string name = input[1];
+                    string phone = input[2];
+
+                    if (!namePhone.ContainsKey(name))
+                    {
+                        namePhone[name] = String.Empty;
+                    }
+
+                    namePhone[name] = phone;
+
+                }
+                else if (input[0] == "S")
+                {
+                    string name = input[1];
+
+                    if (namePhone.ContainsKey(name))
+                    {
+                        Console.WriteLine($"{name} -> {namePhone[name]}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {name} does not exist.");
+                    }
+                }
+            }
+        }
+    }
+}
