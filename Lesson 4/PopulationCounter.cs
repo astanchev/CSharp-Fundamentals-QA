74bdd87f1e867f2d747e3f82bcdc712d4dd46a51
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-06-19 08:41:36 +0300

    Add files via upload

---
+++ Lesson 4/PopulationCounter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _09._Population_Counter
+{
+    class PopulationCounter
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, long>> countryCityPopulation = 
+                new Dictionary<string, Dictionary<string, long>>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "report")
+                {
+                    break;
+                }
+
+                string city = input.Split('|')[0];
+                string country = input.Split('|')[1];
+                int population = int.Parse(input.Split('|')[2]);
+
+                if (!countryCityPopulation.ContainsKey(country))
+                {
+                    countryCityPopulation[country] = new Dictionary<string, long>();
+                }
+
+                if (countryCityPopulation[country].ContainsKey(city))
+                {
+                    countryCityPopulation[country][city] = 0;
+                }
+
+                countryCityPopulation[country][city] = population;
+            }
+
+            foreach (var country in countryCityPopulation
+                                            .OrderByDescending(c => c.Value.Values.Sum()))
+            {
+                Console.WriteLine($"{country.Key} (total population: {country.Value.Values.Sum()})");
+                foreach (var city in country.Value.OrderByDescending(c => c.Value))
+                {
+                    Console.WriteLine($"=>{city.Key}: {city.Value}");
+                }
+            }
+
+        }
+    }
+}
