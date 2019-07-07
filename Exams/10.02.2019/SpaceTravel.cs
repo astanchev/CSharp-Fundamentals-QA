e756c58be4c41823a65d4d0e181fe52d50925e94
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-08 00:10:16 +0300

    Add files via upload

---
+++ Exams/10.02.2019/SpaceTravel.cs
@@ -0,0 +1,35 @@
+﻿namespace _01.SpaceTravel
+{
+    using System;
+    using System.Text;
+
+    class SpaceTravel
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string planet = Console.ReadLine();
+                string message = Console.ReadLine();
+                
+                StringBuilder decodedMessage = new StringBuilder();
+
+                for (int i = 0; i < message.Length; i += 2)
+                {
+                    int charASCII = (message[i] - 48) * 10 + (message[i + 1] - 48);
+
+                    decodedMessage.Append((char)charASCII);
+                }
+
+                Console.WriteLine(decodedMessage);
+
+                if (decodedMessage.ToString() == "GO HOME")
+                {
+                    break;
+                }
+            }
+
+            Console.WriteLine("Going home.");
+        }
+    }
+}
