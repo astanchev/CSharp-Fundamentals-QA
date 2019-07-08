dad2bf63a0f6cde6994f7452f5fc201e5035bccb
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-09 00:05:57 +0300

    Add files via upload

---
+++ Exams/27.01.2019/Usernames.cs
@@ -0,0 +1,33 @@
+﻿namespace _01.Usernames_27January2019
+{
+    using System;
+
+    class Usernames
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                bool isValid = true;
+
+                for (int i = 0; i < input.Length; i++)
+                {
+                    char symb = input[i];
+
+                    if (!char.IsLetter(symb))
+                    {
+                        isValid = false;
+                        Console.WriteLine("Invalid username!");
+                        break;
+                    }
+                }
+
+                if (isValid)
+                {
+                    break;
+                }
+            }
+        }
+    }
+}
