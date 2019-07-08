3a480d1b216b9d7c8b04a2e2ecac162dacae9341
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-09 00:08:18 +0300

    Add files via upload

---
+++ Exams/27.01.2019/Car/Car.cs
@@ -0,0 +1,57 @@
+﻿namespace _04.Car_27January2019
+{
+    public class Car
+    {
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKm;
+        private double traveledDistance;
+
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKm)
+        {
+            this.model = model;
+            this.fuelAmount = fuelAmount;
+            this.fuelConsumptionPerKm = fuelConsumptionPerKm;
+            this.traveledDistance = 0;
+        }
+
+        public string Model
+        {
+            get => this.model;
+            set => this.model = value;
+        }
+        public double FuelAmount
+        {
+            get => this.fuelAmount;
+            set => this.fuelAmount = value;
+        }
+
+        public double FuelConsumptionPerKm
+        {
+            get => this.fuelConsumptionPerKm;
+            set => this.fuelConsumptionPerKm = value;
+        }
+
+        public double TraveledDistance
+        {
+            get => this.traveledDistance;
+            set => this.traveledDistance = value;
+        }
+
+        public bool Drive(double amountOfKm)
+        {
+            double fuelNeedeForTravel = this.FuelConsumptionPerKm * amountOfKm;
+
+            if (this.FuelAmount >= fuelNeedeForTravel)
+            {
+                this.FuelAmount -= fuelNeedeForTravel;
+                traveledDistance += amountOfKm;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Exams/27.01.2019/Car/Startup.cs
@@ -0,0 +1,77 @@
+﻿namespace _04.Car_27January2019
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    
+    class Startup
+    {
+        private static List<Car> cars;
+        static void Main(string[] args)
+        {
+            cars = new List<Car>();
+
+            AddCars();
+
+            DriveCars();
+
+            PrintCars();
+        }
+
+        private static void PrintCars()
+        {
+            foreach (var car in cars)
+            {
+                Console.WriteLine($"{car.Model} {car.FuelAmount:f2} {car.TraveledDistance}");
+            }
+        }
+
+        private static void DriveCars()
+        {
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "End")
+                {
+                    break;
+                }
+
+                string[] inputLine = input.Split();
+
+                string model = inputLine[1];
+                double amountOfKm = double.Parse(inputLine[2]);
+
+                if (cars.Any(c => c.Model == model))
+                {
+                    bool isDrived = cars.First(c => c.Model == model).Drive(amountOfKm);
+
+                    if (!isDrived)
+                    {
+                        Console.WriteLine("Insufficient fuel for the drive");
+                    }
+                }
+            }
+        }
+
+        private static void AddCars()
+        {
+            int numberCars = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberCars; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string model = input[0];
+                double fuelAmount = double.Parse(input[1]);
+                double FuelConsumptionFor1km = double.Parse(input[2]);
+
+                Car car = new Car(model, fuelAmount, FuelConsumptionFor1km);
+
+                if (cars.All(c => c.Model != model))
+                {
+                    cars.Add(car);
+                }
+            }
+        }
+    }
+}
