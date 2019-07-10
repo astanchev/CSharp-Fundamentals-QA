e764208ff0803c8417bd1d651cfcec46261134ab
Атанас Станчев <49180808+astanchev@users.noreply.github.com>
2019-07-10 11:27:06 +0300

    Add files via upload

---
+++ Lesson 6/03. Class Box/Box.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+
+namespace _03._Class_Box
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.length = length;
+            this.width = width;
+            this.height = height;
+        }
+
+        public double GetSurfaceArea()
+        {
+            double area = 2*(length*width) + 2*(length*height) + 2*(height*width);
+
+            return area;
+        }
+        public double GetLateralSurfaceArea()
+        {
+            double lateralArea = 2*(length*height) + 2*(height*width);
+
+            return lateralArea;
+        }
+        public double GetVolume()
+        {
+            double volume = this.length * this.width * this.height;
+
+            return volume;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder();
+
+            result.AppendLine($"Surface Area - {this.GetSurfaceArea():f2}");
+            result.AppendLine($"Lateral Surface Area - {this.GetLateralSurfaceArea():f2}");
+            result.AppendLine($"Volume - {this.GetVolume():f2}");
+
+            return result.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Lesson 6/03. Class Box/StartUpBox.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03._Class_Box
+{
+    public class StartUpBox
+    {
+        public static void Main(string[] args)
+        {
+            double lenght = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+
+            Box box = new Box(lenght, width, height);
+
+            Console.WriteLine(box);
+        }
+    }
+}
